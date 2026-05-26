8f1fd96c69ed6f160b8e2db2afb22117d6c022a2
HenrikGharagyozyan1 <henrik.gharagyozyan1@email.com>
2026-05-26 23:16:48 +0400

    Add modified premake file

--- premake5.lua
+++ premake5.lua
@@ -1,28 +1,52 @@
 project "ImGui"
     kind "StaticLib"
     language "C++"
-    
-    targetdir ("bin/" .. outputdir .. "/%{prj.name}")
-    objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
+    cppdialect "C++17"
+    staticruntime "on"
+
+    targetdir ("../../bin/" .. outputdir .. "/%{prj.name}")
+    objdir ("../../bin-int/" .. outputdir .. "/%{prj.name}")
 
     files
     {
-        "imconfig.h",
-        "imgui.h",
         "imgui.cpp",
+        "imgui_demo.cpp",
         "imgui_draw.cpp",
-        "imgui_internal.h",
+        "imgui_tables.cpp",
         "imgui_widgets.cpp",
-        "imstb_rectpack.h",
-        "imstb_textedit.h",
-        "imstb_truetype.h",
-        "imgui_demo.cpp"
+
+        "backends/imgui_impl_opengl3.cpp",
+        "backends/imgui_impl_glfw.cpp",
+
+        "*.h",
+        "backends/*.h"
     }
-    
-    filter "system:windows"
+
+    includedirs
+    {
+        ".",
+        "../GLFW/include"
+    }
+
+    links
+    {
+        "GLFW",
+        "GL",
+        "dl",
+        "pthread"
+    }
+
+    filter "system:linux"
         systemversion "latest"
-        cppdialect "C++17"
-        staticruntime "On"
-        
-    filter { "system:windows", "configurations:Release" }
-        buildoptions "/MT"
+
+    filter "configurations:Debug"
+        runtime "Debug"
+        symbols "on"
+
+    filter "configurations:Release"
+        runtime "Release"
+        optimize "on"
+
+    filter "configurations:Dist"
+        runtime "Release"
+        optimize "on"
